36c9804a78e4ff42f8fb5569e2654659dd3b980c
dannyshafer <github@danielshafer.com>
2015-07-29 11:05:28 -0700

    make baskets work

--- app/controllers/funds_controller.rb
+++ app/controllers/funds_controller.rb
@@ -1,25 +1,5 @@
 class FundsController < ApplicationController
 
-require 'nokogiri'
-require 'open-uri'
-
-# post '/mutf_summary' do
-#   create_fund(params[:fund])
-
-#     erb :'funds/summary'
-# end
-
-# delete '/funds/:fund_id' do
-#   fund = Fund.find(params[:fund_id])
-#   fund.destroy
-
-#   if request.xhr?
-
-#   else
-#     redirect '/'
-#   end
-# end
-
   def create
     @fund = create_fund(params[:symbol])
     render "funds/show"
